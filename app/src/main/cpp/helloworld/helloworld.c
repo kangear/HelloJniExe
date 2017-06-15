95344e960cfd85afe122abd3a1d1cdc4ae7325d8
Kangear <kangear@163.com>
2017-06-16 00:28:18 +0800

    add logcat support

--- app/src/main/cpp/helloworld/helloworld.c
+++ app/src/main/cpp/helloworld/helloworld.c
@@ -2,6 +2,16 @@
 // Created by tony on 17-6-15.
 //
 #include <stdio.h>
+#include <android/log.h>
+
+#define TAG "HELLOWORLD" // 这个是自定义的LOG的标识
+#define ALOGD(...) __android_log_print(ANDROID_LOG_DEBUG,TAG ,__VA_ARGS__) // 定义LOGD类型
+#define ALOGI(...) __android_log_print(ANDROID_LOG_INFO,TAG ,__VA_ARGS__) // 定义LOGI类型
+#define ALOGW(...) __android_log_print(ANDROID_LOG_WARN,TAG ,__VA_ARGS__) // 定义LOGW类型
+#define ALOGE(...) __android_log_print(ANDROID_LOG_ERROR,TAG ,__VA_ARGS__) // 定义LOGE类型
+#define ALOGF(...) __android_log_print(ANDROID_LOG_FATAL,TAG ,__VA_ARGS__) // 定义LOGF类型
+
 int main() {
     printf("Hello World!\n");
+    ALOGD("Hello Logcat!");
 }
